322f23bd2e8d06a8a34efe37b5fb2b71d7f94a5c
Logan <logan.butler@student.swic.edu>
2020-10-26 22:51:24 -0500

    Added event listener for submit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+const formData = {};
+for (let i = 0; i <= 2; i += 1) {
+  console.log(formData);
+}
