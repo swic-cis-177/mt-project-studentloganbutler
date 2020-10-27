8cc385de6f3daa776a1318e2663609ec7648adb7
Logan <logan.butler@student.swic.edu>
2020-10-27 11:13:19 -0500

    Fixed eventListener

--- scripts.js
+++ scripts.js
@@ -2,9 +2,11 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-});
 
-const formData = {};
-for (let i = 0; i <= 2; i += 1) {
+  const formData = {};
+  for (let i = 0; i <= 2; i += 1) {
+    formData[event.target.elements[i].id] = event.target.elements;
+  }
+
   console.log(formData);
-}
+});
