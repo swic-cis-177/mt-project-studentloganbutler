160ed625efa41711400cfdd9c0cba063a1f9cf26
Logan <logan.butler@student.swic.edu>
2020-12-01 16:37:55 -0600

    Add Activity Table

--- lib.js
+++ lib.js
@@ -1,3 +1,6 @@
+const tbody = document.querySelector("tbody");
+const template = document.querySelector("template");
+
 export const createActivity = (elements) =>
   Array.from(elements)
     // Filter out stuff that has an id
@@ -9,3 +12,27 @@ export const createActivity = (elements) =>
         [id]: value,
       };
     }, {});
+
+export const createActivityTable = (data) => {
+  // For each thing that is in data (object)...
+  data.forEach(
+    (
+      // Destructure (pull out) the 🔑s that we want
+      { name, email, questions }
+    ) => {
+      // Take all of the content in the template tags and copy it
+      const newActivityRow = template.content.cloneNode(true);
+
+      // Make sure to get all 3 tds (not just 1)
+      const newActivityTDs = newActivityRow.querySelector("td");
+
+      // The first td will keep the id
+      newActivityTDs[0].textContent = name;
+      newActivityTDs[1].textContent = email;
+      newActivityTDs[2].textContent = questions;
+
+      // Add the new row inside the tbody
+      tbody.appendChild(newActivityRow);
+    }
+  );
+};
