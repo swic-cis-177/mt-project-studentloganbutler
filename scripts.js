2245a5ebb74d5dcb5a28581169d87767adf130ad
Manav Misra <manavm@visionwebsoft.com>
2020-11-30 19:13:32 -0600

    Add todo comment

--- scripts.js
+++ scripts.js
@@ -7,4 +7,6 @@ form.addEventListener("submit", (event) => {
 
   // Send over the elements that was just submitted
   const formData = createActivity(event.target.elements);
+
+  // TODO: Generate a table that shows (name, email, and questions) (Note to self, student HW will help)
 });
